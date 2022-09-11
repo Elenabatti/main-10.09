042266339dc734e68c9b9277d08ee6233d6670ed
ELENA <battilena@gmail.com>
2022-09-11 12:21:52 +0300

    task26

---
+++ task26/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, котрая принимает на вход число и выдаёт количество цифр в числе.
+
+Console.Clear();
+Console.WriteLine("Введите число А");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+/* string a = Convert.ToString(A);
+Console.WriteLine(a.Length);
+*/
+int count = 0;
+while (A>= 1);
+{
+    count++;
+    A=A/10;
+}
+Console.WriteLine(count);
