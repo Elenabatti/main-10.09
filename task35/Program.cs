7d3ff8dcb950dcf4b80ff6cda676bfd93f407419
ELENA <battilena@gmail.com>
2022-09-12 20:59:42 +0300

    task35

---
+++ task35/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задайте одномерный массив из 123 случайных чисел.
+//Найдите количество элементов массива, значения которых лежат в отрезке [ 10, 99].
+
+Console.Clear();
+Console.WriteLine("Укажите длину массива: ");
+int size = int.Parse(Console.ReadLine());
+int [] rand = new int[size];
+int [] rand2 = new int [size];
+for (int i = 0; i < rand.Length; i++){
+    rand[i] = new Random().Next(-99, 100);
+    Console.WriteLine(rand[i]+" ");
+}
+Console.WriteLine();
+
+
+int x = 0;
+int y = rand.Length;
+while (x == y){
+    rand2[x] = rand[x] * rand[y];
+    x++;
+    y++;
+}
+Console.WriteLine (String.Join(" ", rand2));
+Console.WriteLine();
